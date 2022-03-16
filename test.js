4e5d594b0154418dcd1f67e427d3f1b97484d2e0
Neema Velasco <neemavelasco@gmail.com>
2022-03-16 18:57:41 -0500

    added initial project files, html skeleton, and basic styling to center elements

---
+++ test.js
@@ -0,0 +1,11 @@
+var displayContainer = $('.display-joke'); // contains the joke
+
+
+// hides the display container
+// displayContainer.hide();
+
+// shows the display container
+// displayContainer.show();
+
+// joke api link: https://sv443.net/jokeapi/v2/
+// note: blacklist all inappropriate content
