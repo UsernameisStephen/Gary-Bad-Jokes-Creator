1a035bf2f108bf93d095d0c9ca7007557991f73b
Neema Velasco <neemavelasco@gmail.com>
2022-03-18 10:48:58 -0500

    renamed some elements to clear confusion and added function to hide and show button and joke container

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
+const jokeContainer = $(".display-joke");
+const garyBtnContainer = $(".btn-container");
+const garyHead = $("#gary-head-button");
 
-var displayContainer = $('.display-joke'); // contains the joke
+
+$(document).ready(function() {
+    jokeContainer.hide();
+});
+
 
 
 // hides the display container
@@ -11,3 +18,21 @@ var displayContainer = $('.display-joke'); // contains the joke
 // joke api link: https://sv443.net/jokeapi/v2/
 // note: blacklist all inappropriate content
 
+
+
+
+
+garyHead.on("click", function () {
+
+    garyBtnContainer.hide();
+    jokeContainer.show();
+// console.log(generateJoke());
+// generateJoke();
+});
+
+
+// add this to garyHead click event listener
+const displayJokeContainer = $(".display-joke");
+const btnContainer = $("btn-container");
+btnContainer.hide();
+displayJokeContainer.show();
