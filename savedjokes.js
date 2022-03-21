14566db13e086d4922dba4dd4b1e34eea6244bc2
Jonathan's Mac Mini <jonathansmacmini@Jonathanss-Mini.attlocal.net>
2022-03-21 10:22:14 -0500

    saved joke page

---
+++ savedjokes.js
@@ -0,0 +1,14 @@
+// global variables
+const text_A = document.querySelector('.text-a');
+const text_B = document.querySelector('.text-b');
+const text_C = document.querySelector('.text-c');
+const storedInput = localStorage.getItem('textDisplay');
+
+if(s)
+saveBtn.addEventListener('click' joke => {
+    text.textContent = joke.target.value;
+});
+
+
+//Add local storage save button
+save-btn.addEventListener('click', saveToLocalStorage)
