7e60a4a73c463ec86843a3007079a9a6da7402b1
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-03-22 12:11:59 -0500

    changed sound

--- audio.js
+++ audio.js
@@ -1,9 +1,11 @@
 var garyLogo = $(".gary-logo");
 
 garyLogo.on("click", function () {
-    const music = new Audio('./assets/audio/boy-says-volcano-2439.mp3');
+    const music = new Audio('./assets/audio/waytogogary.mp3');
     music.play();
     setTimeout(function(){
         location.href = "main.html";
-    }, 1500)
+    }, 1300)
 });
+
+
